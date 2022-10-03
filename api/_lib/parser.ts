8433f275e27afd3c968290d55a8539257f8d4acc
codentell <codentell@gmail.com>
2022-10-03 15:19:15 -0700

    fix types

--- api/_lib/parser.ts
+++ api/_lib/parser.ts
@@ -28,7 +28,7 @@ export function parseRequest(req: IncomingMessage) {
 
     const parsedRequest: ParsedRequest = {
         fileType: extension === 'jpeg' ? extension : 'png',
-        theme: theme === 'dark' ? 'dark' : 'light',
+        theme: theme,
         md: md === '1' || md === 'true',
         fontSize: fontSize || '96px',
         images: getArray(images),
