a53b74ad5582e4233c40e4d90dfe958643bff43f
codentell <codentell@gmail.com>
2022-10-04 01:39:02 -0700

    added more backgrounds

--- api/_lib/template.ts
+++ api/_lib/template.ts
@@ -5,12 +5,18 @@ function getCSS(theme: string, fontSize: string) {
     let background = 'white';
     let colorMatrix = [['#FBBC53', '#F49879', '#E649E1'], ['#c2f589', '#77d4ad', '#3aa4a4'], ['#b674ed', '#5f5def', '#6ebceb']];
     let colors = colorMatrix[Math.floor(Math.random() * colorMatrix.length)]
-    let themeBackground = "https://pbs.twimg.com/profile_banners/841458289191747585/1637644132/1500x500";
+    let themeBackground = "https://pbs.twimg.com/profile_banners/1382854043433783296/1654550853/1500x500"; 
     if(theme === "thirdweb"){
-        themeBackground = "https://pbs.twimg.com/profile_banners/1382854043433783296/1654550853/1500x500";
+        themeBackground = "https://pbs.twimg.com/profile_banners/841458289191747585/1637644132/1500x500";
     }
-    if (theme === 'buildspace') {
+    else if (theme === 'buildspace') {
         themeBackground = "https://pbs.twimg.com/profile_banners/1422255111422754816/1632075252/1500x500";
+    } else if(theme === "corgis") {
+        themeBackground = "https://pbs.twimg.com/profile_banners/1452393448628129792/1645558066/1500x500"
+    } else if(theme === "avatar") {
+        themeBackground === "https://pbs.twimg.com/profile_banners/1366011468911497216/1651998820/1500x500"
+    } else if(theme === "dev") {
+        themeBackground === "https://pbs.twimg.com/profile_banners/1433793310703898629/1654088763/1500x500"
     }
     return `
 
